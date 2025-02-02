d1d5b5e5b576e3de6e5bedb5cbad86736541298a
eliezerBrasilian <eliezerassuncaocustodio@hotmail.com>
2025-02-02 15:24:34 -0300

    Update .gitignore, add VSCode settings, and create package configuration

---
+++ package.lua
@@ -0,0 +1,14 @@
+---
+--- Config related to your package
+---
+
+Name = "main_class_generator"
+DirectoryWhereMainFileIs = "com.mainclassgenerator.cryxie.Main"
+Description = ""
+Version = "0.0.1"
+
+RepositoryUrl = ""
+
+Dependencies = {}
+
+Visibility = "public"
--- src/main/java/com/mainclassgenerator/cryxie/Main.java
+++ src/main/java/com/mainclassgenerator/cryxie/Main.java
@@ -1,6 +1,7 @@
 package com.mainclassgenerator.cryxie;
 
- public class Main {
+
+public class Main {
     public static void main(String[] args) {
         new Generator().generate("com.teste.cryxie", directory -> {
             System.out.println("Directory structure created at: " + directory.getAbsolutePath());
